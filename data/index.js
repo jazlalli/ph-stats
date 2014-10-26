f77f61363756f211a8ca3f5eae08b9253a54bfde
Jaz Lalli <jaz@thesandpit.com>
2014-10-26 13:32:33 +0000

    json data files and simply displaying

---
+++ data/index.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var baseDir = 'data/'
+var data = {};
+
+var files = fs.readdirSync(baseDir);
+
+files.forEach(function (file) {
+	var nameparts = file.split('.');
+
+	if (nameparts[1] === 'json') {
+		data[nameparts[0]] = require('./' + file);
+	}
+})
+
+module.exports = data;
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var PORT = process.env.PORT || 3232;
-
+var data = require('./data');
 var express = require('express'),
 		exphbs = require('express-handlebars');
 
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.enable('view cache');
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (request, response) {
-  response.render('index', { stats: 1 });
+  response.render('index', { data: data });
 });
 
 app.listen(PORT, function () {
