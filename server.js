724d28a77dd810869229aec284377c1d28c0ebd5
Jaz Lalli <jaz@thesandpit.com>
2014-10-26 19:17:10 +0000

    split it up

--- server.js
+++ server.js
@@ -4,34 +4,38 @@ var dal = require('./data/adapter');
 var express = require('express'),
 		exphbs = require('express-handlebars');
 
+var userData = {};
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
-var userData = {};
-
 app.get('/', function (req, res) {
-  if (userData.topvoters && userData.topmakers) {
-  	return res.render('index', {data: userData});
-  }
+  res.render('index');
+});
 
-  async.parallel([
-  	function (callback) {
-  		dal.getTopVoters(function (err, voters) {
-  			callback(null, voters);
-  		});
-  	},
-  	function (callback) {
-  		dal.getTopMakers(function (err, makers) {
-  			callback(null, makers);
-  		});
-  	}
-	], function (err, results) {
-		userData.topvoters = results[0];
-		userData.topmakers = results[1];
-		res.render('index', {data: userData});
-	});
+app.get('/topusers', function (req, res) {
+
+  if (userData.topmakers && userData.topvoters) {
+  	res.render('topusers', {data: userData});
+  } else {
+	  async.parallel([
+	  	function (callback) {
+	  		dal.getTopVoters(function (err, voters) {
+	  			callback(null, voters);
+	  		});
+	  	},
+	  	function (callback) {
+	  		dal.getTopMakers(function (err, makers) {
+	  			callback(null, makers);
+	  		});
+	  	}
+		], function (err, results) {
+			userData.topvoters = results[0];
+			userData.topmakers = results[1];
+			res.render('topusers', {data: userData});
+		});
+  }
 });
 
 app.get('/api/data/daysofweek', function (req, res) {
